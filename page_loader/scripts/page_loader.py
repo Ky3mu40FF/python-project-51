a1602c0092c8c3a5739c8a5472ef3a0c8a685ff5
Алексей Фоминых <foma1994@yandex.ru>
2022-09-30 17:39:00 +0300

    CR1. Fix 9-10. In progress...

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -36,7 +36,14 @@ def main():
             args.page_url,
             args.output,
         )
-    except (requests.exceptions.RequestException, OSError):
+    except requests.exceptions.RequestException as req_exc:
+        print('Error occured while downloading. {0}'.format(req_exc))
+        sys.exit(1)
+    except OSError as os_exc:
+        print('Error occured while saving to file. {0}\n{1}'.format(os_exc.strerror, os_exc.filename))
+        sys.exit(1)
+    except Exception as exc:
+        print('Unexpected error occured. {0}'.format(exc))
         sys.exit(1)
     print('Page {0} successfully saved to {1}'.format(
         args.page_url,
